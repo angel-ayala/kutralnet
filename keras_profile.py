489019ebe54b334ef5f31697ac8e7053ef91b40d
Angel Ayala <angel4ayala@gmail.com>
2020-01-27 19:30:11 -0300

    keras flops measure (unstable)

--- keras_profile.py
+++ keras_profile.py
@@ -1,10 +1,6 @@
-import os.path
-import tempfile
 import tensorflow as tf
 import keras.backend as K
-from tensorflow.python.keras import Model, Sequential
 from models.firenet_tf import firenet_tf
-# import numpy as np
 
 def get_flops(model):
     run_meta = tf.RunMetadata()
@@ -21,5 +17,11 @@ model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-# .... Define your model here ....
+print(get_flops(model))
+
+model = firenet_tf(input_shape=(64, 64, 3))
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
 print(get_flops(model))
